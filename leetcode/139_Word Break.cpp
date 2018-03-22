dbfb803f825e7d010af2ae4dc79c62ffa9d144e1
Zhang Han <463808243@qq.com>
2018-03-22 11:12:09 +0800

    dp 一题

---
+++ leetcode/139_Word Break.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<stack>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+	bool wordBreak(string s, vector<string>& wordDict) {
+		int len = s.size();
+		if (len < 1) return false;
+		vector<bool> dp(len+1, false); //dp[i]: 从下标0开始，长度为i的字符串是否true
+		unordered_map<string, int> str2num;
+		for (auto e : wordDict)
+			str2num[e] = 1;
+		dp[0] = true;
+		for (int i = 1; i <= len; i++)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				dp[i] = dp[j] && (str2num.count(s.substr(j, i - j))>0);
+				if (dp[i]) break;
+			}
+		}
+		return dp[len];
+	}
+};
+
+int main()
+{
+	Solution solu;
+	vector<int> vec = { 100, 4, 200, 1, 3, 2 };
+	
+	system("pause");
+	return 0;
+}
