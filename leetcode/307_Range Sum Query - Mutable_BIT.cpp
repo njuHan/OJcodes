1a3a2ecca0bdb85e387e44923cf1486346fc048d
Zhang Han <463808243@qq.com>
2018-04-10 23:54:08 +0800

    307 use binary indexed tree

---
+++ leetcode/307_Range Sum Query - Mutable_BIT.cpp
@@ -0,0 +1,55 @@
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<map>
+#include<unordered_map>
+using namespace std;
+
+class NumArray {
+public:
+	NumArray(vector<int> nums) {
+		n = nums.size();
+		c = vector<int>(n + 1, 0);
+		for (int i = 0; i < n; i++)
+			add(i + 1, nums[i]);
+	}
+
+	void update(int i, int val) {
+		int d = val - sumRange(i, i);
+		add(i + 1, d);
+	}
+
+	int sumRange(int i, int j) {
+		return sum(j+1) - sum(i);
+	}
+private:
+	vector<int> c;
+	int n;
+	int lowbit(int x)
+	{
+		return x & (-x);
+	}
+	int sum(int x)
+	{
+		int ans = 0;
+		while (x>0)
+		{
+			ans += c[x];
+			x = x - lowbit(x);
+		}
+		return ans;
+	}
+	void add(int x, int d)
+	{
+		while (x<=n)
+		{
+			c[x] += d;
+			x = x + lowbit(x);
+		}
+	}
+};
+
