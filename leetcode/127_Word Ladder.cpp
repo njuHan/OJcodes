f4c1491b9ec028f3763160805963396952ca4907
Zhang Han <463808243@qq.com>
2018-04-13 22:50:55 +0800

    bfs

---
+++ leetcode/127_Word Ladder.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cstdio>
+#include<queue>
+#include<unordered_set>
+
+using namespace std;
+class Solution {
+public:
+	int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+		wordDict = unordered_set<string>(wordList.begin(), wordList.end());
+		toVisit.push(beginWord);
+		cnt = 1;
+		while (!toVisit.empty())
+		{
+			int len = toVisit.size();
+			for (int i = 0; i < len; i++)
+			{
+				string word = toVisit.front(); toVisit.pop();
+				if (word == endWord) return cnt;
+				addWord(word);
+			}
+			cnt++;
+		}
+		return 0;
+	}
+private:
+	int cnt;
+	queue<string> toVisit;
+	unordered_set<string> wordDict;
+	void addWord(string word)
+	{
+		wordDict.erase(word);
+		for (int i = 0; i < word.size(); i++)
+		{
+			string temp(word);
+			for (int j = 0; j < 26; j++)
+			{
+				temp[i] = j + 'a';
+				if (wordDict.count(temp))
+				{
+					toVisit.push(temp);
+					wordDict.erase(temp);
+				}
+			}
+		}
+	}
+};
+
