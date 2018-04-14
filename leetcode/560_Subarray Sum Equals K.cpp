dfd24484363dcd3faccfdca636fbaa1451af328b
Zhang Han <463808243@qq.com>
2018-04-14 21:19:29 +0800

    prefix sum

---
+++ leetcode/560_Subarray Sum Equals K.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<queue>
+#include<functional>
+#include<bitset>
+#include<map>
+using namespace std;
+
+class Solution {
+public:
+	int subarraySum2(vector<int>& nums, int k) {
+		int len = nums.size();
+		vector<int> sum(len + 1, 0);
+		for (int i = 1; i <= len; i++)
+			sum[i] = sum[i - 1] + nums[i - 1];
+		int ans = 0;
+		for (int i = 0; i < len; i++)
+			for (int j = i + 1; j <= len; j++)
+				if (sum[j] - sum[i] == k)
+					ans++;
+		return ans;
+	}
+	//prefix map faster
+	int subarraySum(vector<int>& nums, int k) {
+		int len = nums.size();
+		int ans = 0;
+		map<int, int> sumCnt;
+		sumCnt[0]++;
+		int sum = 0;
+		for (int i = 0; i < len; i++)
+		{
+			sum += nums[i];
+			if (sumCnt.count(sum - k)) ans += sumCnt[sum-k];
+			sumCnt[sum]++;
+		}
+		return ans;
+
+	}
+};
