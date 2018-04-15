9fe284fda739b1306e323b1e492bdb28fe521104
Zhang Han <463808243@qq.com>
2018-04-15 19:07:49 +0800

    tree

---
+++ leetcode/572_Subtree of Another Tree.cpp
@@ -0,0 +1,34 @@
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<sstream>
+#include<numeric>
+#include<bitset>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	bool isSubtree(TreeNode* s, TreeNode* t) {
+		return cmp(s, t) || (s != NULL && (isSubtree(s->left, t) || isSubtree(s->right, t)));
+	}
+	bool cmp(const TreeNode* lhs, const TreeNode* rhs)
+	{
+		if ((lhs == NULL && rhs != NULL) || (lhs != NULL && rhs == NULL)) return false;
+		if (lhs == NULL && rhs == NULL) return true;
+		return lhs->val == rhs->val && cmp(lhs->left, rhs->left) && cmp(lhs->right, rhs->right);
+	}
+};
+
