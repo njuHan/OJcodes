b9f8dc05d0d88043d8635f1b147dd3c0154b04b8
Zhang Han <463808243@qq.com>
2018-04-17 11:07:22 +0800

    update

---
+++ algorithms/求二进制1的个数.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<stack>
+#include<algorithm>
+using namespace std;
+
+
+int cntOne1(int x)
+{
+	int cnt = 0;
+	while (x!=0)
+	{
+		cnt++;
+		x = x&(x - 1);
+	}
+	return cnt;
+}
+//思想：用1（1自身左移运算，其实后来就不是1了）和n的每位进行位与，来判断1的个数
+int cntOne2(int x)
+{
+	int cnt = 0;
+	int flag = 1;
+	while (flag!=0)
+	{
+		if (flag&x)
+			cnt++;
+		flag = flag << 1;
+	}
+	return cnt;
+}
+int cntOne3(int x)
+{
+	int cnt = 0;
+	unsigned int temp = (unsigned int)x;
+	while (temp!=0)
+	{
+		if (temp & 1) cnt++;
+		temp = temp >> 1;
+	}
+	return cnt;
+}
+
+int main()
+{
+	int x = -30;
+	cout << cntOne1(x) << endl;
+	cout << cntOne2(x) << endl;
+	cout << cntOne3(x) << endl;
+	system("pause");
+}
