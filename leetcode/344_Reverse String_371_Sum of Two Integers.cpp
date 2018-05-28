3aeaf19647ff0e2bf3d09de3e2e9d80ff9a325e6
Zhang Han <463808243@qq.com>
2018-05-28 23:03:29 +0800

    update

---
+++ leetcode/344_Reverse String_371_Sum of Two Integers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reverseString(string s) {
+        return string(s.rbegin(),s.rend());
+    }
+};
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        return a+b;
+    }
+};
