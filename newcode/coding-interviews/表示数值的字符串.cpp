6de93d410053d43ee08ffe59354d2204b47b72f2
Zhang Han <463808243@qq.com>
2018-06-12 16:54:12 +0800

    update

--- newcode/coding-interviews/表示数值的字符串.cpp
+++ newcode/coding-interviews/表示数值的字符串.cpp
@@ -1,44 +1,20 @@
 #include<iostream>
 #include<cstdio>
 #include<vector>
-#include<set>
-#include<map>
-#include<unordered_map>
+#include<queue>
+#include<stack>
 #include<string>
+#include<unordered_map>
+#include<unordered_set>
+#include<set>
+#include<list>
 #include<sstream>
 using namespace std;
+
 class Solution {
 public:
-	//DFA
-	bool isNumber(string str) {
-		int state = 0;
-		while (str[0] == ' ')  str.erase(0, 1);//delete the  prefix whitespace 
-		while (str[str.length() - 1] == ' ') str.erase(str.length() - 1, 1);//delete the suffix whitespace
-		for (int i = 0; i<str.length(); i++) {
-			if ('0' <= str[i] && str[i] <= '9') {
-				if (state <= 2) state = 2;
-				else state = (state <= 5) ? 5 : 7;
-			}
-			else if ('+' == str[i] || '-' == str[i]) {
-				if (state == 0 || state == 3) state++;
-				else return false;
-			}
-			else if ('.' == str[i]) {
-				if (state < 2) state = 6;
-				else if (state == 2) state = 7;
-				else return false;
-			}
-			else if ('e' == str[i]) {
-				if (state == 2  || state == 7) state = 3;
-				else return false;
-			}
-			else return false;
-		}
-		return (state == 2 || state == 5 || state == 7);
-	}
-	// use if
-	bool isNumber_2(string str) {
-        const char* string = str.c_str();
+	bool isNumeric(char* string)
+	{
 		int len = strlen(string), i = 0;
 		if (len < 1) return false;
 		//skip the leading spaces
@@ -74,13 +50,13 @@ public:
 		//skip trailing spaces
 		while (i < len && string[i] == ' ') i++;
 		return i == len;
-    }
+
+	}
+
 };
 
 int main()
 {
 	Solution solu;
-	
-	system("pause");
-	return 0;
+	solu.isNumeric("0");
 }
