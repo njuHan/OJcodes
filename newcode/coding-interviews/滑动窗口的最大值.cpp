a3b00331889f6bc2b4d6b85ef838b985e6dde618
Zhang Han <463808243@qq.com>
2018-06-13 11:20:57 +0800

    update

---
+++ newcode/coding-interviews/滑动窗口的最大值.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+#include<set>
+#include<list>
+#include<ctime>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> maxInWindows(const vector<int>& num, unsigned int size)
+	{
+		int len = num.size(), k = size; 
+		vector<int> ans;
+		if (len < 1 || size < 1) return ans;
+		deque<int> dq;
+		for (int i = 0; i < len; i++)
+		{
+			while (!dq.empty() && dq.front() <= i - k) dq.pop_front();
+			while (!dq.empty() && num[dq.back()] < num[i]) dq.pop_back();
+			dq.push_back(i);
+			if (i >= k - 1) ans.emplace_back(num[dq.front()]);
+		}
+		return ans;
+	}
+};
+
+int main()
+{
+	unsigned int a = 3;
+	int b = 1;
+	cout << b - a << endl; //×ªÎÞ·ûºÅÊý cout<< 4294967294
+	system("pause");
+	return 0;
+}
