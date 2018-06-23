8591e41a92aee3628f16c8d691a8c5ddf0a14050
Zhang Han <463808243@qq.com>
2018-06-23 22:39:23 +0800

    update

---
+++ leetcode/377_Combination Sum IV.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+#include<set>
+#include<list>
+#include<sstream>
+#include<functional>
+using namespace std;
+
+// https://leetcode.com/problems/combination-sum-iv/discuss/85097/What-if-negative-numbers-are-allowed-in-the-given-array
+// https://leetcode.com/problems/combination-sum-iv/discuss/85120/C++-template-for-ALL-Combination-Problem-Set
+class Solution {
+public:
+	int combinationSum4(vector<int>& nums, int target) {
+		if (nums.empty()) return 0;
+		vector<int> dp(target + 1, 0);
+		dp[0] = 1;
+		for (int i = 1; i <= target; i++)
+		{
+			for (int num : nums)
+				if (i >= num) dp[i] += dp[i - num];
+		}
+		return dp[target];
+	}
+};
