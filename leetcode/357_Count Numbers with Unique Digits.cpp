e6b1627a0671abd4097ad660878ef703a00a75e3
Zhang Han <463808243@qq.com>
2018-07-08 22:18:40 +0800

    update

--- leetcode/357_Count Numbers with Unique Digits.cpp
+++ leetcode/357_Count Numbers with Unique Digits.cpp
@@ -16,8 +16,11 @@ class Solution {
 public:
 	int countNumbersWithUniqueDigits(int n) {
 		if (n <= 0) return 1;
-		// ans: 位数为1到n的UniqueDigits 的个数
-		//pre : 位数为n的 niqueDigits 的个数
+		// ans[n]: 位数为0到n的UniqueDigits 的个数, 把0看作位数为0
+		//pre[n] : 位数为n且第一位不为0的 niqueDigits 的个数
+		//n==0, 1特殊情况
+		// n==0时，只有 一个 0， ans[0] = 1;
+		// n==1时，pre[1]=9, ans[1] = pre[1] + ans[0] = 9+1 =10;
 		int ans = 10, pre = 9; //n==1;
 		for (int i = 2; i <= min(10, n); i++)
 		{
