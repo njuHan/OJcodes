489ce12d41c8b528964e0facae16d3926005be3c
Zhang Han <463808243@qq.com>
2018-08-14 16:26:13 +0800

    backtracking

--- leetcode/017_Letter Combinations of a Phone Number.cpp
+++ leetcode/017_Letter Combinations of a Phone Number.cpp
@@ -4,6 +4,38 @@
 #include<string>
 using namespace std;
 
+// update 2018-08-14, backtracking
+class Solution {
+public:
+	vector<string> letterCombinations(string digits) {
+		search(0, "", digits);
+		return ans;
+	}
+private:
+	vector<string> ans;
+	static  vector<string> dig2let;
+	void search(int cur, string str, string& digits)
+	{
+		if (cur == digits.length() && str.length()) 
+        {
+            ans.emplace_back(str);
+            return;
+        }
+		int dig = digits[cur] - '0';
+		if (dig < 1 || dig>9) return search(cur + 1, str, digits);
+		for (char ch : dig2let[dig])
+		{
+			str.push_back(ch);
+			search(cur + 1, str, digits);
+			str.pop_back();
+		}
+	}
+};
+vector<string> Solution::dig2let = { "","","abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+
+
+// 以下是几个月前写的，感觉不行
+
 //这题类似BFS，一开始想到用queue，但是觉得队列取个元素还要两个操作麻烦
 //其实用vector自己控制访问顺序就行了
 //另外vector的深拷贝和浅拷贝了解下
