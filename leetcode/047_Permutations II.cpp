bbeebd927836276a87ca84af7639edf49d90094a
Zhang Han <463808243@qq.com>
2018-08-15 13:12:22 +0800

    update permutation

--- leetcode/047_Permutations II.cpp
+++ leetcode/047_Permutations II.cpp
@@ -8,6 +8,61 @@
 #include<unordered_set>
 using namespace std;
 
+//使用swap, 每次从剩余元素中选取一个添加到排列
+class Solution {
+public:
+	vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+		pick(0, nums);
+		return ans;
+	}
+private:
+	vector<vector<int>> ans;
+    // 这里nums 不能引用，因为cur+1之后的操作会打乱cur时的nums的顺序，导致重复访问
+	void pick(int cur, vector<int> nums)
+	{
+        
+		if (cur == nums.size()) { ans.emplace_back(nums); return; }
+		for (int i = cur; i < nums.size(); i++)
+		{
+			if (i > cur && nums[i] == nums[cur]) continue;
+			swap(nums[i], nums[cur]);
+			pick(cur + 1, nums);
+		}
+	}
+};
+
+
+// backtracking
+class Solution2 {
+public:
+	vector<vector<int>> permuteUnique(vector<int>& nums) {
+		int n = nums.size();
+        sort(nums.begin(), nums.end());
+		vis = vector<bool>(n, 0);
+		vector<int> vec = vector<int>(n, 0);
+		search(0, vec, nums);
+		return ans;
+	}
+private:
+	vector<bool> vis;
+	vector<vector<int>> ans;
+	void search(int cur, vector<int>& arr, vector<int>& nums)
+	{
+		if (cur == nums.size()) { ans.emplace_back(arr); return; }
+		for (int i = 0; i < nums.size(); i++)
+		{
+			//  这里避免相同元素重复访问，首先得排序使得相同元素相邻
+			if (vis[i] || i != 0 && nums[i - 1] == nums[i] && vis[i - 1] == 0) continue;
+			vis[i] = 1;
+			arr[cur] = nums[i];
+			search(cur+1, arr, nums);
+			vis[i] = 0;
+		}
+	}
+};
+
+
 class Solution {
 public:
 	vector<vector<int>> permuteUnique(vector<int>& nums) {
