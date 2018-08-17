4331696224029a608effb425222aac1e285f821d
Zhang Han <463808243@qq.com>
2018-08-17 10:43:23 +0800

    update

--- leetcode/064_Minimum Path Sum.cpp
+++ leetcode/064_Minimum Path Sum.cpp
@@ -6,6 +6,39 @@
 #include<string>
 using namespace std;
 
+//O(n) space,  bottom to top 
+class Solution3 {
+public:
+	int minPathSum(vector<vector<int>>& grid) {
+		if (grid.empty()) return 0;
+		int m = grid.size(), n = grid[0].size();
+		vector<int> dp(n+1, INT_MAX); //多出最后一列，int_max
+		dp[n - 1] = 0;
+		for (int i=m-1; i>=0; i--)
+			for (int j = n - 1; j >= 0; j--)
+				dp[j] = min(dp[j + 1], dp[j]) + grid[i][j];
+		return dp[0];
+	}
+};
+
+//O(n) space, top to bottom
+class Solution2 {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        if (grid.empty()) return 0;
+		int m = grid.size(), n = grid[0].size();
+		vector<int> dp(n, grid[0][0]);
+		for (int j = 1; j < n; j++) dp[j] = dp[j - 1] + grid[0][j];
+		for (int i = 1; i < m; i++)
+		{
+			dp[0] += grid[i][0]; //dp[i][0] = dp[i-1][0] + grid[i][0]
+			for (int j = 1; j < n; j++)
+				dp[j] = min(dp[j - 1], dp[j]) + grid[i][j];
+		}
+		return dp[n - 1];
+    }
+};
+
 class Solution {
 public:
 	int minPathSum(vector<vector<int>>& grid) {
