3528621203245ffa0c6806a38fca30e3e98521a0
Zhang Han <463808243@qq.com>
2018-08-17 19:19:01 +0800

    update

--- leetcode/376_Wiggle Subsequence.cpp
+++ leetcode/376_Wiggle Subsequence.cpp
@@ -6,6 +6,26 @@
 #include<algorithm>
 using namespace std;
 
+class Solution2 {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int n = nums.size();
+        if (n<1) return 0;
+        int up = 1, down = 1;
+        for (int i=1; i<n; i++)
+        {	
+			//证明 nums[i-1]和 nums[i]之间的大小关系，可以保证 up down 序列衔接正确
+			// 如果(nums[i-1]<nums[i]) 且 nums[i-1]结尾是下降的，那么加上nums[i]，以nums[i]结尾上升：满足 up = down + 1;
+			// 如果(nums[i-1]<nums[i]) 且 nums[i-1]结尾是上升的，那么必定存在 j< i-1 && nums[j]<nums[i-1] => nums[j]<nums[i], 
+			// nums[j]作用于down, 加上nums[i]，以nums[i]结尾上升：满足 up = down + 1;
+			// 同理可知 down = up + 1 正确
+            if (nums[i-1]<nums[i]) up = down + 1; 
+            else if (nums[i-1]>nums[i]) down = up+1;
+        }
+        return max(up, down);
+    }
+};
+
 class Solution {
 public:
 	int wiggleMaxLength(vector<int>& nums) {
