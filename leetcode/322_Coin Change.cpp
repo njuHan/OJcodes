6e3a5be60011818407765d069110e8a2488a5a8d
Zhang Han <463808243@qq.com>
2018-08-18 17:51:41 +0800

    knapsack

--- leetcode/322_Coin Change.cpp
+++ leetcode/322_Coin Change.cpp
@@ -12,6 +12,18 @@ using namespace std;
 
 class Solution {
 public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1, amount+1);
+        dp[0] = 0;
+        for (int i=1; i<=amount; i++)
+            for (int e : coins)
+                if (i>=e) dp[i] = min(dp[i], dp[i-e]+1);
+        return dp[amount]>amount ? -1 : dp[amount];
+    }
+};
+
+class Solution {
+public:
 	int coinChange(vector<int>& coins, int amount) {
 		vector<int> dp(amount + 1, INT_MAX);
 		// dp[i] 兑换i元需要的最少硬币
