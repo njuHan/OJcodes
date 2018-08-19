57739ee53b2aa0bcaee56004779bab9fc85dbeaa
Zhang Han <463808243@qq.com>
2018-08-19 18:17:03 +0800

    update edit dist

--- leetcode/072_Edit Distance.cpp
+++ leetcode/072_Edit Distance.cpp
@@ -6,6 +6,24 @@
 #include<string>
 using namespace std;
 
+class Solution2 {
+public:
+	int minDistance(string word1, string word2) {
+		int m = word1.length(), n = word2.length();
+		vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+		for (int i = 0; i <= m; i++) dp[i][0] = i;
+		for (int j = 0; j <= n; j++) dp[0][j] = j;
+		for (int i=1; i<=m; i++)
+			for (int j = 1; j <= n; j++)
+			{
+				if (word1[i - 1] == word2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+				//                  É¾³ý           ÐÞ¸Ä                  Ìí¼Ó
+				else dp[i][j] = min({dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]}) + 1;
+			}
+		return dp[m][n];
+	}
+};
+
 class Solution {
 public:
 	int minDistance(string word1, string word2) {
