1e30d14c52a6a49e47690586fadf44a4931c4641
Zhang Han <463808243@qq.com>
2018-08-19 10:54:48 +0800

    update stock 1/2

--- leetcode/121_Best Time to Buy and Sell Stock.cpp
+++ leetcode/121_Best Time to Buy and Sell Stock.cpp
@@ -5,6 +5,20 @@
 #include<algorithm>
 using namespace std;
 
+class Solution2 {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int pft = 0, buy = INT_MAX; 
+        for (int p : prices)
+        {
+            pft = max(pft, p - buy);
+            buy = min(p, buy);
+        }
+        return pft;
+    }
+};
+
 class Solution {
 public:
 	int maxProfit(vector<int>& prices) 
--- leetcode/377_Combination Sum IV.cpp
+++ leetcode/377_Combination Sum IV.cpp
@@ -12,6 +12,17 @@
 #include<functional>
 using namespace std;
 
+class Solution2 {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1, 0); dp[0] = 1;
+        for (int i=1; i<=target; i++)
+            for (int num : nums)
+                dp[i] += (i>=num)*dp[i-num];
+        return dp[target];
+    }
+};
+
 // https://leetcode.com/problems/combination-sum-iv/discuss/85097/What-if-negative-numbers-are-allowed-in-the-given-array
 // https://leetcode.com/problems/combination-sum-iv/discuss/85120/C++-template-for-ALL-Combination-Problem-Set
 class Solution {
