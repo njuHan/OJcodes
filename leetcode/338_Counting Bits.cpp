d4aa2343096e57bf1f3c6a466eabf89cab93d15e
Zhang Han <463808243@qq.com>
2018-08-25 10:46:59 +0800

    bit manipulation

--- leetcode/338_Counting Bits.cpp
+++ leetcode/338_Counting Bits.cpp
@@ -6,6 +6,24 @@ using namespace std;
 
 class Solution {
 public:
+	// faster
+	vector<int> countBits(int num) {
+        vector<int> dp(num+1, 0);
+        for (int i=1; i<=num; i++)
+            dp[i] = dp[i>>1] + (i&1);
+        return dp;
+    }
+
+    vector<int> countBits(int num) {
+        vector<int> dp(num+1, 0);
+        for (int i=1; i<=num; i++)
+            dp[i] = dp[i&(i-1)] + 1;
+        return dp;
+    }
+};
+
+class Solution {
+public:
 	vector<int> countBits(int num) {
 		vector<int> ans;
 		if (num < 0) return ans;
