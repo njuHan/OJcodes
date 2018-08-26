2d2395622c203aa0c12dd9cead70528dcf18c7d9
Zhang Han <463808243@qq.com>
2018-08-26 17:38:35 +0800

    update

--- newcode/coding-interviews/数值的整数次方.cpp
+++ newcode/coding-interviews/数值的整数次方.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
 	double Power(double base, int exponent) {
+		if (exponent == 0 || base==1) return 1;
+		if (exponent < 0) return 1 / base*Power(1 / base, -(exponent + 1)); //注意INT_MIN溢出
+		return exponent & 1 ? base*Power(base*base, exponent / 2) : Power(base*base, exponent / 2);
+	}
+};
+
+
+class Solution {
+public:
+	double Power(double base, int exponent) {
 		if (exponent == 0 || base == 1) return 1;
 		if (exponent < 0) return 1/base * Power(1 / base, -(exponent+1));
 		return exponent % 2 ? base*Power(base*base, exponent / 2) : Power(base*base, exponent / 2);
--- newcode/coding-interviews/调整数组顺序使奇数位于偶数前面.cpp
+++ newcode/coding-interviews/调整数组顺序使奇数位于偶数前面.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+	void reOrderArray(vector<int> &array) {
+		int n = array.size();
+		// 冒泡排序的思想，注意按位与 & 的优先级小于 == ，要加括号
+		for (int k = n-1; k >= 0; k--)
+		{
+			if ( (array[k]&1) == 0)
+			{
+				for (int i = k, j = k + 1; j < n && (array[j] & 1); i++, j++)
+					swap(array[i], array[j]);
+			}
+		}
+	}
+};
+
+class Solution {
+public:
     void reOrderArray(vector<int> &array) {
         int len = array.size();
 		if (len <= 1) return;
