503f58259f11f48ae168385d9118188538e03ad5
Zhang Han <463808243@qq.com>
2018-08-27 15:02:08 +0800

    update

--- newcode/coding-interviews/和为S的连续正数序列.cpp
+++ newcode/coding-interviews/和为S的连续正数序列.cpp
@@ -1,33 +1,15 @@
-#include<iostream>
-#include<cstdio>
-#include<vector>
-#include<queue>
-#include<stack>
-#include<string>
-#include<unordered_map>
-#include<unordered_set>
-#include<set>
-#include<list>
-#include<sstream>
-using namespace std;
-
 
 class Solution {
 public:
 	vector<vector<int> > FindContinuousSequence(int sum) {
 		if (sum < 2) return ans; //at least 2 numbers
-		int lo = 1, hi = 1, mid = sum / 2, cur = 1;
-		vector<int> vec = { 1 };
+		int lo = 1, hi = 2, mid = sum / 2, cur = 3;
 		while (lo<=mid)
 		{
+            if (cur == sum) pushAns(lo, hi);
 			cur += ++hi;
-			vec.push_back(hi);
-			if (cur == sum) pushAns(lo, hi);
 			while (lo<=mid && cur>sum)
-			{
 				cur -= lo++;
-				if (cur == sum) pushAns(lo, hi);
-			}
 		}
 		return ans;
 	}
@@ -40,12 +22,3 @@ private:
 		ans.emplace_back(vec);
 	}
 };
-
-int main()
-{
-	Solution solu;
-	vector<vector<int>> ans = solu.FindContinuousSequence(100);
-
-	system("pause");
-	return 0;
-}
--- newcode/coding-interviews/孩子们的游戏(圆圈中最后剩下的数).cpp
+++ newcode/coding-interviews/孩子们的游戏(圆圈中最后剩下的数).cpp
@@ -1,28 +1,17 @@
-#include<iostream>
-#include<cstdio>
-#include<vector>
-#include<queue>
-#include<stack>
-#include<string>
-#include<unordered_map>
-#include<unordered_set>
-#include<set>
-#include<list>
-#include<ctime>
-using namespace std;
-
-
 class Solution {
 public:
-	//f(n,m) = (f(n-1,m) + m) % n;
-	int LastRemaining_Solution(int n, int m)
-	{
-		if (n < 1 || m < 1) return -1;
+    int LastRemaining_Solution(int n, int m)
+    {
+        if (n < 1 || m < 1) return -1;
+        //n==1时， 只有一个 0号， last = 0
+        // n==i-1时的 0 号， 对应与 n==i时的 m 号
+        // 所以从 i-1 推到 i :  last = (last + m)%i
+		//f(n,m) = (f(n-1,m) + m) % n;
 		int last = 0;
 		for (int i = 2; i <= n; i++)
 		{
 			last = (last + m) % i;
 		}
 		return last;
-	}
+    }
 };
